d9c82c51c3c635c46b2b1fae40fc331978f3c5b9
Nestor Luis Bermúdez <nestorluisbermudez@gmail.com>
2020-12-09 20:48:48 -0300

    Corregido

--- ui-stencil/ui/src/components/rigth-panel/rigth-panel.tsx
+++ ui-stencil/ui/src/components/rigth-panel/rigth-panel.tsx
@@ -113,10 +113,10 @@ export class RigthPanel {
         <aside class="sidebar">
           <div class="order">
             <h4>Tu orden</h4>
-            {this.order.products.map(product => (
+            {this.order.products.map((product, index) => (
               <p id="pedido">
-                {product.description} ${product.price}
-                <a href="#link" class="btn btn-outline-info" role="button"onClick={()=>this.deleteProduct(product.id)}>
+                {product.name} {product.description} ${product.price}
+                <a href="#link" class="btn btn-outline-info" role="button"onClick={()=>this.deleteProduct(index)}>
                 <svg width="0.7em" height="0.7em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
                 </svg>
